7ffdd18769c31e22ab15e52c2ff3a034fc04406b
Forrest <forrest.shi@gmail.com>
2014-01-09 14:40:16 +0800

    Initial Commit

---
+++ MyPodsDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyPodsDemo
+//
+//  Created by Shi Lin on 1/9/14.
+//  Copyright (c) 2014 Forrest Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
