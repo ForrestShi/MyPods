e1fcbb76b1108679aaa4236117030f0a4869cb3c
Forrest <forrest.shi@gmail.com>
2014-01-09 15:38:15 +0800

    add MyClass

---
+++ MyClass/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  BGView.h
+//  ViewHierarchDemo
+//
+//  Created by Shi Lin on 1/8/14.
+//  Copyright (c) 2014 Forrest Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
